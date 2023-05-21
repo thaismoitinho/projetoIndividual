8e01f1b6aa8f5cbceda86ddb082ae44e06136a29
thaismoitinho <thais.moitinho@sptech.school>
2023-05-21 16:15:23 -0300

    subindo atualização pagina de usuario

--- site/public/funcoes.js
+++ site/public/funcoes.js
@@ -4,7 +4,7 @@ function validarSessao() {
 
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
-
+    console.log(nome)
     var b_usuario = document.getElementById("b_usuario");
 
     if (email != null && nome != null) {
@@ -13,7 +13,7 @@ function validarSessao() {
 
         // finalizarAguardar();
     } else {
-        window.location = "../login.html";
+        window.location = "login.html";
     }
 }
 
@@ -21,7 +21,7 @@ function limparSessao() {
     // aguardar();
     sessionStorage.clear();
     // finalizarAguardar();
-    window.location = "../login.html";
+    window.location = "login.html";
 }
 
 // carregamento (loading)
